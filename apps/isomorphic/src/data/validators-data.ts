b22917c04553271e8f63d9189a6b63864e467849
nvagon <matiasagnetii@gmail.com>
2025-09-03 21:12:23 +0900

    Update Home Page

---
+++ apps/isomorphic/src/data/validators-data.ts
@@ -0,0 +1,52 @@
+export type ValidatorDataType = {
+  uid: number;
+  hotkey: string;
+  weight: number;
+  updated: number;
+  version: number;
+};
+
+export const validatorsData: ValidatorDataType[] = [
+  {
+    uid: 195,
+    hotkey: "Web Agents - Autoppia",
+    weight: 1722467.28,
+    updated: 556,
+    version: 7,
+  },
+  {
+    uid: 13,
+    hotkey: "tao5",
+    weight: 217234.77,
+    updated: 262,
+    version: 4,
+  },
+  {
+    uid: 3,
+    hotkey: "RoundTable21",
+    weight: 200261.91,
+    updated: 1836,
+    version: 5,
+  },
+  {
+    uid: 71,
+    hotkey: "Yuma, a DCG Company",
+    weight: 143629.17,
+    updated: 203,
+    version: 5,
+  },
+  {
+    uid: 46,
+    hotkey: "Kraken",
+    weight: 90782,
+    updated: 193,
+    version: 2,
+  },
+  {
+    uid: 181,
+    hotkey: "5G1NjW9YhXLadMWajvTkfcJy6up3yH2q1YzMXDTi6ijanChe",
+    weight: 51105.44,
+    updated: 77,
+    version: 5,
+  },
+];
