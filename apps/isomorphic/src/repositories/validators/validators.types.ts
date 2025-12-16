14e0035827a499745cf9e250014ca11755be57bd
Miguel <developer151086@gmail.com>
2025-12-16 21:51:38 +0100

    add validators page and redirect from overview

---
+++ apps/isomorphic/src/repositories/validators/validators.types.ts
@@ -0,0 +1,92 @@
+/**
+ * Validator Details API Types
+ */
+
+export interface ValidatorInfo {
+  uid: number;
+  hotkey: string | null;
+  stake: number | null;
+  weight: number | null;
+  lastRoundEvaluated: number | null;
+  image: string | null;
+}
+
+export interface GlobalStats {
+  totalEvaluations: number;
+  successCount: number;
+  zeroCount: number;
+  nullCount: number;
+  failedCount: number;
+  successPct: number;
+  zeroPct: number;
+  nullPct: number;
+  failedPct: number;
+}
+
+export interface ContextInfo {
+  lastRoundWinner: number | null;
+  lastRoundWinnerReward: number | null;
+  lastRoundWinnerWeight: number | null;
+  lastRoundWinnerName: string | null;
+  lastRoundWinnerImage: string | null;
+  lastRoundWinnerHotkey: string | null;
+}
+
+export interface TaskStats {
+  taskId: string;
+  taskPrompt: string;
+  totalEvaluations: number;
+  successCount: number;
+  zeroCount: number;
+  nullCount: number;
+  failedCount: number;
+  successPct: number;
+  zeroPct: number;
+  nullPct: number;
+  failedPct: number;
+}
+
+export interface UseCaseStats {
+  useCaseName: string;
+  useCaseId: string;
+  totalEvaluations: number;
+  successCount: number;
+  zeroCount: number;
+  nullCount: number;
+  failedCount: number;
+  successPct: number;
+  zeroPct: number;
+  nullPct: number;
+  failedPct: number;
+  tasks: TaskStats[];
+}
+
+export interface WebStats {
+  webName: string;
+  webId: string;
+  totalEvaluations: number;
+  successCount: number;
+  zeroCount: number;
+  nullCount: number;
+  failedCount: number;
+  successPct: number;
+  zeroPct: number;
+  nullPct: number;
+  failedPct: number;
+  useCases: UseCaseStats[];
+}
+
+export interface ValidatorDetailsData {
+  validator: ValidatorInfo;
+  globalStats: GlobalStats;
+  context: ContextInfo;
+  webs: WebStats[];
+  validatorImage: string | null;
+  availableRounds: number[];
+}
+
+export interface ValidatorDetailsResponse {
+  success: boolean;
+  data: ValidatorDetailsData;
+}
+
